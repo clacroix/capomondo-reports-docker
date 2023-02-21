51e07091a8cdb3b37c6f7f15dca5326d9938c48b
Christian Lacroix <clacroix@mac.com>
2023-02-21 14:47:08 +0100

    added daily cron script

--- cron.sh
+++ cron.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker run --rm -v /home/clacroix/capomondo-v3/files:/srv/files --env-file /home/clacroix/capomondo-v3/capomondo-reports-docker/.env.php.local -it ghcr.io/clacroix/capomondo-reports-cli:latest bin/console capomondo:import
-docker run --rm -v /home/clacroix/capomondo-v3/files:/srv/files --env-file /home/clacroix/capomondo-v3/capomondo-reports-docker/.env.php.local -it ghcr.io/clacroix/capomondo-reports-cli:latest bin/console capomondo:cron daily
+docker run --rm -v /home/clacroix/capomondo-v3/files:/srv/files --env-file /home/clacroix/capomondo-v3/capomondo-reports-docker/.env.php.local -it ghcr.io/clacroix/capomondo-reports-cli:latest bin/console capomondo:cron daily --email
 
